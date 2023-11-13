a0640d298f69a6182a5136ed1d461dcc04afc19c
Ronan LE MEILLAT <rtkgps@outlook.com>
2023-11-13 16:50:01 +0100

    add tools for our kubernetes

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var moment = require('moment');
 var basicAuth = require('express-basic-auth');
 var http = require('http');
 var https = require('https');
-
+var oktetoToolkit = require('@sctg/okteto-auto-token');
+var yaml = require('js-yaml');
 var path = require('path');
 var mime = require('mime-types');
 var fs = require('fs');
@@ -27,30 +28,30 @@ var BASIC_AUTH_USER = process.env.BASIC_AUTH_USER;
 var BASIC_AUTH_PWD = process.env.BASIC_AUTH_PWD;
 
 if (BASIC_AUTH_USER && BASIC_AUTH_PWD) {
-    app.use(function(req, res, next) {
-        res.setHeader('WWW-Authenticate', 'Basic realm="Restricted Area"')
-        next();
-    });
+	app.use(function (req, res, next) {
+		res.setHeader('WWW-Authenticate', 'Basic realm="Restricted Area"')
+		next();
+	});
 
 	app.use(basicAuth({
-        users: {
-            [BASIC_AUTH_USER]: BASIC_AUTH_PWD
-        }
-    }))
+		users: {
+			[BASIC_AUTH_USER]: BASIC_AUTH_PWD
+		}
+	}))
 }
 // ssl credentials
 var credentials = {
-  key: process.env.SSL_KEY ? fs.readFileSync(process.env.SSL_KEY) : '',
-  cert: process.env.SSL_CERT ? fs.readFileSync(process.env.SSL_CERT) : '',
+	key: process.env.SSL_KEY ? fs.readFileSync(process.env.SSL_KEY) : '',
+	cert: process.env.SSL_CERT ? fs.readFileSync(process.env.SSL_CERT) : '',
 }
 
 if (
-  (credentials.key && !credentials.cert) ||
-  (credentials.cert && !credentials.key)
+	(credentials.key && !credentials.cert) ||
+	(credentials.cert && !credentials.key)
 ) {
-    console.error('Please provide both SSL_KEY and SSL_CERT');
-    process.exit(1);
-  }
+	console.error('Please provide both SSL_KEY and SSL_CERT');
+	process.exit(1);
+}
 
 var startHttpsServer = credentials.key && credentials.cert;
 
@@ -64,7 +65,7 @@ app.set('view engine', 'ejs');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+	extended: true
 }));
 app.use(busboy()); // to support file uploads
 
@@ -82,17 +83,17 @@ app.set('host', (process.env.HOST || '0.0.0.0'));
 app.set('port', (process.env.PORT || 8000));
 
 // root page handler
-app.get(routes.root, function(req, res) {
-  // reload the database before rendering
+app.get(routes.root, function (req, res) {
+	// reload the database before rendering
 	crontab.reload_db();
 	// send all the required parameters
-	crontab.crontabs( function(docs){
+	crontab.crontabs(function (docs) {
 		res.render('index', {
-			routes : JSON.stringify(routes_relative),
-			crontabs : JSON.stringify(docs),
-			backups : crontab.get_backup_names(),
-			env : crontab.get_env(),
-      moment: moment
+			routes: JSON.stringify(routes_relative),
+			crontabs: JSON.stringify(docs),
+			backups: crontab.get_backup_names(),
+			env: crontab.get_env(),
+			moment: moment
 		});
 	});
 });
@@ -102,52 +103,52 @@ Handle to save crontab to database
 If it is a new job @param _id is set to -1
 @param name, command, schedule, logging has to be sent with _id (if exists)
 */
-app.post(routes.save, function(req, res) {
+app.post(routes.save, function (req, res) {
 	// new job
-	if(req.body._id == -1){
+	if (req.body._id == -1) {
 		crontab.create_new(req.body.name, req.body.command, req.body.schedule, req.body.logging, req.body.mailing);
 	}
 	// edit job
-	else{
+	else {
 		crontab.update(req.body);
 	}
 	res.end();
 });
 
 // set stop to job
-app.post(routes.stop, function(req, res) {
+app.post(routes.stop, function (req, res) {
 	crontab.status(req.body._id, true);
 	res.end();
 });
 
 // set start to job
-app.post(routes.start, function(req, res) {
+app.post(routes.start, function (req, res) {
 	crontab.status(req.body._id, false);
 	res.end();
 });
 
 // remove a job
-app.post(routes.remove, function(req, res) {
+app.post(routes.remove, function (req, res) {
 	crontab.remove(req.body._id);
 	res.end();
 });
 
 // run a job
-app.post(routes.run, function(req, res) {
+app.post(routes.run, function (req, res) {
 	crontab.runjob(req.body._id);
 	res.end();
 });
 
 // set crontab. Needs env_vars to be passed
-app.get(routes.crontab, function(req, res, next) {
-	crontab.set_crontab(req.query.env_vars, function(err) {
+app.get(routes.crontab, function (req, res, next) {
+	crontab.set_crontab(req.query.env_vars, function (err) {
 		if (err) next(err);
 		else res.end();
 	});
 });
 
 // backup crontab db
-app.get(routes.backup, function(req, res) {
+app.get(routes.backup, function (req, res) {
 	crontab.backup((err) => {
 		if (err) next(err);
 		else res.end();
@@ -155,32 +156,32 @@ app.get(routes.backup, function(req, res) {
 });
 
 // This renders the restore page similar to backup page
-app.get(routes.restore, function(req, res) {
+app.get(routes.restore, function (req, res) {
 	// get all the crontabs
-	restore.crontabs(req.query.db, function(docs){
+	restore.crontabs(req.query.db, function (docs) {
 		res.render('restore', {
-			routes : JSON.stringify(routes_relative),
-			crontabs : JSON.stringify(docs),
-			backups : crontab.get_backup_names(),
+			routes: JSON.stringify(routes_relative),
+			crontabs: JSON.stringify(docs),
+			backups: crontab.get_backup_names(),
 			db: req.query.db
 		});
 	});
 });
 
 // delete backup db
-app.get(routes.delete_backup, function(req, res) {
+app.get(routes.delete_backup, function (req, res) {
 	restore.delete(req.query.db);
 	res.end();
 });
 
 // restore from backup db
-app.get(routes.restore_backup, function(req, res) {
+app.get(routes.restore_backup, function (req, res) {
 	crontab.restore(req.query.db);
 	res.end();
 });
 
 // export current crontab db so that user can download it
-app.get(routes.export, function(req, res) {
+app.get(routes.export, function (req, res) {
 	var file = crontab.crontab_db_file;
 
 	var filename = path.basename(file);
@@ -194,7 +195,7 @@ app.get(routes.export, function(req, res) {
 });
 
 // import from exported crontab db
-app.post(routes.import, function(req, res) {
+app.post(routes.import, function (req, res) {
 	var fstream;
 	req.pipe(req.busboy);
 	req.busboy.on('file', function (fieldname, file, filename) {
@@ -208,7 +209,7 @@ app.post(routes.import, function(req, res) {
 });
 
 // import from current ACTUALL crontab
-app.get(routes.import_crontab, function(req, res) {
+app.get(routes.import_crontab, function (req, res) {
 	crontab.import_crontab();
 	res.end();
 });
@@ -221,19 +222,19 @@ function sendLog(path, req, res) {
 }
 
 // get the log file a given job. id passed as query param
-app.get(routes.logger, function(req, res) {
+app.get(routes.logger, function (req, res) {
 	let _file = crontab.log_folder + "/" + req.query.id + ".log";
 	sendLog(_file, req, res);
 });
 
 // get the log file a given job. id passed as query param
-app.get(routes.stdout, function(req, res) {
+app.get(routes.stdout, function (req, res) {
 	let _file = crontab.log_folder + "/" + req.query.id + ".stdout.log";
 	sendLog(_file, req, res);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
 	var data = {};
 	var statusCode = err.statusCode || 500;
 
@@ -250,106 +251,129 @@ app.use(function(err, req, res, next) {
 	res.status(statusCode).json(data);
 });
 
-process.on('SIGINT', function() {
-  console.log("Exiting crontab-ui");
-  process.exit();
+// okteto helpers
+const clustersFile = process.env.CLUSTERS_FILE;
+if (typeof (clustersFile) !== "undefined") {
+	app.get('/kube.config', async (req, res) => {
+		res.setHeader('content-type', 'text/plain');
+		const clusters = oktetoToolkit.readClustersJson(clustersFile)
+		const kubeconfig = await oktetoToolkit.getKubeconfig(clusters);
+		res.send(yaml.dump(kubeconfig));
+	});
+	app.get('/kube.json', async (req, res) => {
+		res.setHeader('content-type', 'application/json');
+		const clusters = oktetoToolkit.readClustersJson(clustersFile)
+		const kubeconfig = await oktetoToolkit.getKubeconfig(clusters);
+		res.send(JSON.stringify(kubeconfig,null,2));
+	  });
+	  
+	  app.get('/kube.sh', async (req, res) => {
+		res.setHeader('content-type', 'text/plain');
+		const clusters = oktetoToolkit.readClustersJson(clustersFile)
+		res.send(oktetoToolkit.getBashTokens(clusters));
+	  });
+}
+
+process.on('SIGINT', function () {
+	console.log("Exiting crontab-ui");
+	process.exit();
 })
 
-process.on('SIGTERM', function() {
-  console.log("Exiting crontab-ui");
-  process.exit();
+process.on('SIGTERM', function () {
+	console.log("Exiting crontab-ui");
+	process.exit();
 })
 
 var server = startHttpsServer ?
-  https.createServer(credentials, app) : http.createServer(app);
+	https.createServer(credentials, app) : http.createServer(app);
 
 // Cloudcmd
 const socket = new Server(server, {
-    path: `${CloudcmdPrefix}socket.io`
+	path: `${CloudcmdPrefix}socket.io`
 });
 
 const config = {
-    name: 'Cloud Commander',
+	name: 'Cloud Commander',
 	oneFilePanel: true,
-	online:true,
+	online: true,
 	contact: false,
 	vim: true,
 	root: '/opt/cron',
 	configAuth: false,
 	terminal: false,
-	syncConsolePath:true
+	syncConsolePath: true
 };
 
 const filePicker = {
-    data: {
-        FilePicker: {
-            key: 'key',
-        },
-    },
+	data: {
+		FilePicker: {
+			key: 'key',
+		},
+	},
 };
 
 // override option from json/modules.json
 const modules = {
-    filePicker,
+	filePicker,
 };
 
 const {
-    createConfigManager,
-    configPath,
+	createConfigManager,
+	configPath,
 } = cloudcmd;
 
 const configManager = createConfigManager({
-    configPath,
+	configPath,
 });
 
 app.use(CloudcmdPrefix, cloudcmd({
-    socket, // used by Config, Edit (optional) and Console (required)
-    config, // config data (optional)
-    modules, // optional
-    configManager, // optional
+	socket, // used by Config, Edit (optional) and Console (required)
+	config, // config data (optional)
+	modules, // optional
+	configManager, // optional
 }));
 // /Cloudcmd
 
-server.listen(app.get('port'), app.get('host'), function() {
-  console.log("Node version:", process.versions.node);
-  fs.access(crontab.db_folder, fs.W_OK, function(err) {
-    if(err){
-      console.error("Write access to", crontab.db_folder, "DENIED.");
-      process.exit(1);
-    }
-  });
-  // If --autosave is used then we will also save whatever is in the db automatically without having to mention it explictly
-  // we do this by watching log file and setting a on change hook to it
-  if (process.argv.includes("--autosave") || process.env.ENABLE_AUTOSAVE) {
-    crontab.autosave_crontab(()=>{});
-    fs.watchFile(crontab.crontab_db_file, () => {
-      crontab.autosave_crontab(()=>{
-        console.log("Attempted to autosave crontab");
-      });
-    });
-  }
-  if (process.argv.includes("--reset")){
-    console.log("Resetting crontab-ui");
-    var crontabdb = crontab.crontab_db_file;
-    var envdb = crontab.env_file;
+server.listen(app.get('port'), app.get('host'), function () {
+	console.log("Node version:", process.versions.node);
+	fs.access(crontab.db_folder, fs.W_OK, function (err) {
+		if (err) {
+			console.error("Write access to", crontab.db_folder, "DENIED.");
+			process.exit(1);
+		}
+	});
+	// If --autosave is used then we will also save whatever is in the db automatically without having to mention it explictly
+	// we do this by watching log file and setting a on change hook to it
+	if (process.argv.includes("--autosave") || process.env.ENABLE_AUTOSAVE) {
+		crontab.autosave_crontab(() => { });
+		fs.watchFile(crontab.crontab_db_file, () => {
+			crontab.autosave_crontab(() => {
+				console.log("Attempted to autosave crontab");
+			});
+		});
+	}
+	if (process.argv.includes("--reset")) {
+		console.log("Resetting crontab-ui");
+		var crontabdb = crontab.crontab_db_file;
+		var envdb = crontab.env_file;
 
-    console.log("Deleting " + crontabdb);
-    try{
-      fs.unlinkSync(crontabdb);
-    } catch (e) {
-      console.log("Unable to delete " + crontabdb);
-    }
+		console.log("Deleting " + crontabdb);
+		try {
+			fs.unlinkSync(crontabdb);
+		} catch (e) {
+			console.log("Unable to delete " + crontabdb);
+		}
 
-    console.log("Deleting " + envdb);
-    try{
-      fs.unlinkSync(envdb);
-    } catch (e) {
-      console.log("Unable to delete " + envdb);
-    }
+		console.log("Deleting " + envdb);
+		try {
+			fs.unlinkSync(envdb);
+		} catch (e) {
+			console.log("Unable to delete " + envdb);
+		}
 
-    crontab.reload_db();
-  }
+		crontab.reload_db();
+	}
 
-  var protocol = startHttpsServer ? "https" : "http";
-  console.log("Crontab UI (" + package_json.version + ") is running at " + protocol + "://" + app.get('host') + ":" + app.get('port') + base_url);
+	var protocol = startHttpsServer ? "https" : "http";
+	console.log("Crontab UI (" + package_json.version + ") is running at " + protocol + "://" + app.get('host') + ":" + app.get('port') + base_url);
 });
